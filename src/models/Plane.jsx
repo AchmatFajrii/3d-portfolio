88e3f18cd704fdd0f67b7b5746c7cd25b81252d1
HeyFajri <achmatfajri@gmail.com>
2023-12-25 03:40:34 +1300

    update bird, plane, sky

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import planeScane from "../assets/3d/plane.glb";
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 
 const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
   const { scene, animations } = useGLTF(planeScane);
+  const { actions } = useAnimations(animations, scene);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [actions, isRotating]);
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={ref}>
       <primitive object={scene} />
     </mesh>
   );
