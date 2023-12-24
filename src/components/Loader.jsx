05e580f247a8f400868ac33578fd28c9a3de2042
HeyFajri <achmatfajri@gmail.com>
2023-12-25 04:54:45 +1300

    update loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,16 @@
 import { Html } from "@react-three/drei";
+import "../styles/loaders.css";
 
 const Loader = () => {
   return (
     <Html>
-      <div className="flex justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+      <div class="🤚">
+        <div class="👉"></div>
+        <div class="👉"></div>
+        <div class="👉"></div>
+        <div class="👉"></div>
+        <div class="🌴"></div>
+        <div class="👍"></div>
       </div>
     </Html>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
