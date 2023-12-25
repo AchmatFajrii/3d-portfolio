f55fa4fd0b13393a82f379c548a6c8091c372444
HeyFajri <achmatfajri@gmail.com>
2023-12-26 05:01:51 +1300

    add contact, email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,126 @@
+import { useState, useRef } from "react";
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    emailjs
+      .send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+
+        {
+          from_name: form.name,
+          to_name: "Fajri",
+          from_email: form.email,
+          to_email: "achmatfajri@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        const message = "Success! Your message has been sent";
+        enqueueSnackbar(message, {
+          variant: "success",
+          anchorOrigin: {
+            vertical: "top",
+            horizontal: "right",
+          },
+        });
+        setForm({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        enqueueSnackbar(error.text, {
+          variant: "error",
+          anchorOrigin: {
+            vertical: "top",
+            horizontal: "right",
+          },
+        });
+      });
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <SnackbarProvider />
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+        >
+          <label htmlFor="name" className="text-black-500 font-semibold">
+            Name
+            <input
+              id="name"
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John Doe"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label htmlFor="email" className="text-black-500 font-semibold">
+            Email
+            <input
+              id="email"
+              type="text"
+              name="email"
+              className="input"
+              placeholder="john@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label htmlFor="message" className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              id="message"
+              type="text"
+              name="message"
+              className="textarea"
+              placeholder="Let me know how I can help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
