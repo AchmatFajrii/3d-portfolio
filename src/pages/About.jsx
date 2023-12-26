5085399f66000b45125b00a656095f87aef8c3b8
HeyFajri <achmatfajri@gmail.com>
2023-12-26 15:45:03 +1300

    add skills section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,42 @@
+import { skills } from "../constants";
+
 const About = () => {
-  return <div>INI ABOUTT</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradiend_text font-semibold drop-shadow">
+          Fajri
+        </span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Front End Developer based in Indonesia, specializing in technical
+          education through hands-on learning and building apllicarions
+        </p>
+
+        <div className="py-10 flex flex-col">
+          <h3 className="subhead-text">My Skills</h3>
+
+          <div className="mt-16 flex flex-wrap gap-12">
+            {skills.map((skill) => (
+              <div className="block-container w-20 h-20">
+                <div className="btn-back rounded-xl" />
+                <div className="btn-front rounded-xl flex justify-center items-center">
+                  <img
+                    src={skill.imageUrl}
+                    alt={skill.name}
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
