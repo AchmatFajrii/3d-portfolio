c50f6fb236f8be6d374ff293db6cb91d7eeeca23
HeyFajri <achmatfajri@gmail.com>
2023-12-26 16:40:39 +1300

    add cta

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -61,10 +61,12 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
     if (e.key === "ArrowLeft") {
       if (!isRotating) setIsRotating(true);
       islandRef.current.rotation.y += 0.01 * Math.PI;
+      rotationSpeed.current = 0.0125;
     }
     if (e.key === "ArrowRight") {
       if (!isRotating) setIsRotating(true);
       islandRef.current.rotation.y -= 0.01 * Math.PI;
+      rotationSpeed.current = -0.0125;
     }
   };
 
