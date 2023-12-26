8c207e6a97caf19908008437bdf65e0fccd5c6a3
HeyFajri <achmatfajri@gmail.com>
2023-12-26 16:48:20 +1300

    fix overflow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
   const [planeScale, planePosition] = adjustPlanedForScreenSize();
 
   return (
-    <section className="w-full h-screen relative">
+    <section className="w-full h-screen relative overflow-y-hidden">
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         {currentStage && <HomeInfo currentStage={currentStage} />}
       </div>
